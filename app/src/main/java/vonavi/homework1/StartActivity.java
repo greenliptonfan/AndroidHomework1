785d976f120e96e545be7c069fbfb481abfb1b0c
vonavi <greenliptonfan@gmail.com>
2015-10-18 17:18:49 +0300

    removed imports

--- app/src/main/java/vonavi/homework1/StartActivity.java
+++ app/src/main/java/vonavi/homework1/StartActivity.java
@@ -1,16 +1,11 @@
 package vonavi.homework1;
 
-        import android.content.Intent;
-        import android.os.Message;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.view.Menu;
-        import android.view.MenuItem;
-        import android.app.Activity;
-        import android.widget.ImageView;
-        import android.os.AsyncTask;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
 
-        import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Валентин on 11.10.2015.
@@ -22,11 +17,8 @@ public class StartActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-
-
     mt = new MyTask();
     mt.execute();
-
     }
 
     @Override
